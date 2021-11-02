974a59ee7c629ebdfb4ec502c64de0e1cc44060f
Metin-Trampov <63451415+MetinTrampov@users.noreply.github.com>
2021-11-02 18:26:32 +0200

    Add files via upload

---
+++ Ехе №7.php
@@ -0,0 +1,39 @@
+<?php
+
+$n=5;
+  $value = 1;
+  for($rows = 1; $rows <= $n; $rows++) 
+  {
+    for($star = 1; $star <= $rows; $star++) {
+      echo "$value "; 
+      $value++;
+    }
+    echo "</br>"; 
+  }
+ 
+ echo "</br>";
+ 
+$n2=10;
+  $value2 = 1;
+  for($rows = 1; $rows <= $n2; $rows++) 
+  {
+    for($star = 1; $star <= $rows; $star++) {
+      echo "$value2 "; 
+      $value2++;
+    }
+    echo "</br>"; 
+  }
+  
+  echo "</br>";
+  
+  $n3=10;
+  $value3 = 1;
+  for($rows = 1; $rows <= $n3; $rows++) 
+  {
+      
+    for($star = 1; $star <= $rows; $star++) {
+      echo "$value3 "; 
+      $value3++;
+    }
+    echo "</br>"; 
+  }
